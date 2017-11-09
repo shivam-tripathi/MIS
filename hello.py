6920e203a5d76a3041637f395953bf1a62bb85e1
Shivam Tripathi <shivam.flash@gmail.com>
2017-11-09 16:15:26 +0530

    active work

--- hello.py
+++ hello.py
@@ -27,6 +27,13 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
 class Check:
     def __init__(self, one, two, three):
