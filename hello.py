a06775d72ecf8bdbeb6f7661d8ac37b0408c1880
Shivam Tripathi <shivam.flash@gmail.com>
2017-11-09 11:45:36 +0530

    Hello - 1

--- hello.py
+++ hello.py
@@ -7,5 +7,20 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
+class Check:
+    def __init__(self, one, two, three):
+        self.one = one
+        self.two = two
+        self.three = three
+
+    def get_alpha(self):
+        return self.one
+
+@app.route('/check')
+def check():
+    obj = Check(1, "Two", [i for i in range(10)])
+    print (obj.three, obj.two)
+    return render_template('check.html', obj=obj, lis=obj.three, str=obj.two, user=obj.two)
+
 if __name__ == '__main__':
     app.run(debug=True)
