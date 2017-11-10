5c2342ceffa3ac777f07b359b95716f013f15bc7
Shivam Tripathi <shivam.flash@gmail.com>
2017-11-10 12:59:16 +0530

    Run signin and signup forms

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '3ad05b627be2e0258a6a67ebccf54db633847c76'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class SignupForm(FlaskForm):
+    name = StringField('Username', validators=[Required(), Length(min=1, max=10), Regexp("^[\w]{3,16}$")])
+    pwd = PasswordField('Password', validators=[Required(), Length(min=5, max=25)])
+    cnfpwd = PasswordField('Confirm Password', validators=[Required(), Length(min=5, max=25)])
+    email = StringField('Email', validators=[Required(), Email()])
+    submit = SubmitField('Submit')
+
+class SigninForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Email()])
+    pwd = PasswordField('Password', validators=[Required(), Length(min=5, max=25)])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -22,6 +37,7 @@ def internal_server_error(e):
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html',
                            current_time=datetime.utcnow())
@@ -31,13 +47,29 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = SignupForm()
+    if request.method == 'GET':
+        return render_template('signup.html', form=form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            return redirect(url_for('index'))
+        else:
+            return '<h1> Invalid submission </h1> %s %s %s %s' % (form.name.data, form.pwd.data, form.cnfpwd.data, form.errors)
 
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template('signin.html')
+    form = SigninForm()
+    if request.method == 'GET':
+        print ('GET method')
+        return render_template('signin.html', form = form)
+    elif request.method == 'POST':
+        print ('POST method')
+        if form.validate_on_submit():
+            return redirect(url_for('index'))
+        else:
+            return '<h1> Invalid submission %s %s %s </h1>' % (form.name.data, form.email.data, form.errors)
 
 class Check:
     def __init__(self, one, two, three):
